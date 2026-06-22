65ba16e4530a6609f0cbad60fc6d9a3dd235c36a
Sara Nadkar <nadkarsara4@gmail.com>
2026-06-22 20:38:03 +0530

    Implement patient retrieval using path parameters

---
+++ 03_Path_Query_Params/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI,path
+import json
+from pathlib import Path
+
+app = FastAPI()
+
+BASE_DIR = Path("D:/FastAPI_Learning/02_HTTP_methods")
+
+def load_load():
+    with open(BASE_DIR/'patient.json','r') as f:
+        data = json.load(f)
+    return data
+
+@app.get("/")
+def hello():
+    return {"message":"Patient management syatem API."}
+
+@app.get("/about")
+def about():
+    return{"message":"A functional API to manage your patient records."}
+
+@app.get("/view")
+def view():
+    data = load_load()
+    return data
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id: str = path(..., description="ID of the patient in DB",example ="P001" )):
+    # load the data
+    data = load_load()
+
+    if patient_id in data:
+        return data[patient_id]
+    return {"error":"patient not found"}
