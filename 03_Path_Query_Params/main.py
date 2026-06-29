63e4d0fe0bde50de1ee538898708eafc33b7ab87
Sara Nadkar <nadkarsara4@gmail.com>
2026-06-29 18:25:49 +0530

    Add HTTPException for invalid patient ID

--- 03_Path_Query_Params/main.py
+++ 03_Path_Query_Params/main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI,Path
+from fastapi import FastAPI,Path,HTTPException
 import json
-from pathlib import Path
+from pathlib import Path as filepath
 
 app = FastAPI()
 
-BASE_DIR = Path("D:/FastAPI_Learning/02_HTTP_methods")
+BASE_DIR = filepath("D:/FastAPI_Learning/02_HTTP_methods")
 
 def load_data():
     with open(BASE_DIR/'patient.json','r') as f:
@@ -31,4 +31,4 @@ def view_patient(patient_id: str = Path(..., description="ID of the patient in D
 
     if patient_id in data:
         return data[patient_id]
-    return {"error":"patient not found"}
+    raise HTTPException(status_code = 404, detail ="Patient not found")
