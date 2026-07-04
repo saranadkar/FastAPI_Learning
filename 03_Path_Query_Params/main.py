107e6a237405025b917382103eade16be5fba282
Sara Nadkar <nadkarsara4@gmail.com>
2026-07-04 10:46:58 +0530

    Add POST endpoint for creating new patients

--- 03_Path_Query_Params/main.py
+++ 03_Path_Query_Params/main.py
@@ -1,15 +1,53 @@
 from fastapi import FastAPI,Path,HTTPException,Query
+from fastapi.responses import JSONResponse
 import json
 from pathlib import Path as filepath
+from pydantic import BaseModel,Field,computed_field
+from typing import Annotated,Literal
 
 app = FastAPI()
 
-BASE_DIR = filepath("D:/FastAPI_Learning/02_HTTP_methods")
+class Patient(BaseModel):
+    id:Annotated[str, Field(..., description="ID of the patient",examples=['P001'])]
+    name:Annotated[str, Field(..., description="Name of the patient")]
+    city:Annotated[str, Field(..., description="City of the patient where he/she lives")]
+    age:Annotated[int, Field(..., gt=0, lt=120, description="Age of the patient")]
+    gender:Annotated[Literal ['male','female','other'],Field(..., description="Gender of the patient")]
+    height:Annotated[float, Field(..., gt=0,description="Height of the patient")]
+    weight:Annotated[float, Field(..., gt=0,description="weight of the patient")]
+
+    @computed_field
+    @property
+    def bmi(self)->float:
+        bmi = round(self.weight/(self.height**2),2)
+        return bmi
+    
+    @computed_field
+    @property
+    def verdict(self) ->str:
+
+        if self.bmi < 18.5:
+            return "Underweight"
+
+        elif self.bmi < 25:
+            return "Normal"
+
+        elif self.bmi < 30:
+            return "Overweight"
+
+        else:
+            return "Obese"
+            
+BASE_DIR = filepath("D:/FastAPI_Learning/03_Path_Query_Params")
 
 def load_data():
     with open(BASE_DIR/'patient.json','r') as f:
         data = json.load(f)
     return data
+
+def save_data(data):
+    with open(BASE_DIR/'patient.json','w') as f:
+        json.dump(data,f)
 
 @app.get("/")
 def hello():
@@ -51,3 +89,21 @@ def sort_patient(sort_by: str = Query(..., description="Sort on the basis of hei
     sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by,0), reverse=sort_order)
 
     return sorted_data
+
+@app.post("/create")
+def create_patient(patient:Patient):
+
+    #load existing data
+    data = load_data()
+
+    #check if patient already exists
+    if patient.id in data:
+        raise HTTPException(status_code=400, detail='Patient already exist')
+
+    #add new patient in DB
+    data[patient.id] = patient.model_dump(exclude=['id'])   # dumps is used to convert into dictionary
+
+    #save into json file
+    save_data(data)
+
+    return JSONResponse(status_code=201, content={'message':'patient created successfully'})
